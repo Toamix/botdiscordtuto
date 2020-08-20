41b927de511d85c4b8c31d681b5e96dcb206b909
Toamix <69990815+Toamix@users.noreply.github.com>
2020-08-20 23:04:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    "TOKEN" : "NzMxOTUyNzk3NTQ4ODA2MjA0.XwtiKQ.huFCI7SSnbkOBCeBCLAPp7BMA1U",
-    "PREFIX" : "!",
+    "TOKEN" : "",
+    "PREFIX" : "",
 }
 module.exports = config;
